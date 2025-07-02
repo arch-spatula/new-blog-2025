03f8313895be4e688b6bcf6674fdf89bd2ba9923
김상현 <84452145+arch-spatula@users.noreply.github.com>
2025-07-02 21:27:44 +0900

    feat: 마크다운 읽고 html로 파싱하기 별도 함수로 분리 #3

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,43 @@
 import "./style.css";
-import readme from "../README.md?raw";
-import test from "./test.md?raw";
+// import readme from "../README.md?raw";
+// import test from "./test.md?raw";
 
 import { unified } from "unified";
 import markdown from "remark-parse";
 import remark2rehype from "remark-rehype";
 import html from "rehype-stringify";
 
-const main = async () => {
+const markdownToHtml = (markdownSource: string) => {
   const html_text = unified()
     .use(markdown)
     .use(remark2rehype)
     .use(html)
-    .processSync(readme);
+    .processSync(markdownSource);
+
+  if (typeof html_text.value === "string") {
+    return html_text.value;
+  }
+
+  return "";
+};
+
+const main = async () => {
+  const readme = await import("../content/2025-07-02/2025-07-02-page.md?raw");
 
   const app = document.querySelector<HTMLDivElement>("#app");
 
   if (app) {
     const button = document.createElement("button");
     button.textContent = "hello old school";
+    app.appendChild(button);
 
     const markdownContainer = document.createElement("div");
     markdownContainer.classList.add("markdown-body");
-    if (typeof html_text.value === "string") {
-      markdownContainer.insertAdjacentHTML("beforeend", html_text.value);
-    }
+    markdownContainer.insertAdjacentHTML(
+      "beforeend",
+      markdownToHtml(readme.default),
+    );
 
-    app.appendChild(button);
     app.appendChild(markdownContainer);
   }
 };
