09586d0b5ca130cd4585f819206011d6b096aeee
김상현 <84452145+arch-spatula@users.noreply.github.com>
2025-07-05 12:51:47 +0900

    refactor: 파일 분리 #3

---
+++ scripts/generate.ts
@@ -0,0 +1,30 @@
+import * as path from "path";
+import * as fs from "fs";
+import findMarkdownFiles from "./findMarkdownFiles";
+import markdownToHtml from "./markdownToHtml";
+
+const generate = () => {
+  const srcDir = path.resolve(__dirname, "content"); // *.md 모아둔 곳
+  const outDir = path.resolve(__dirname, "public/blog");
+
+  // 1) public/blog 디렉터리 초기화
+  fs.rmSync(outDir, { recursive: true, force: true });
+  fs.mkdirSync(outDir, { recursive: true });
+
+  // 2) 모든 markdown 찾기
+  const mdFiles = findMarkdownFiles(srcDir);
+
+  // 3) 변환 & 저장
+  for (const mdFile of mdFiles) {
+    const markdown = fs.readFileSync(mdFile, "utf8");
+    const htmlfileStr = markdownToHtml(markdown);
+
+    const outPath = mdFile
+      .replace(/\.md$/, ".html")
+      .replace("content", "public/blog");
+    fs.mkdirSync(path.dirname(outPath), { recursive: true });
+    fs.writeFileSync(outPath, htmlfileStr, "utf8");
+  }
+};
+
+export default generate;
