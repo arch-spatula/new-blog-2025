e19652ddd22d5c472126e6fc002d020a40216799
김상현 <84452145+arch-spatula@users.noreply.github.com>
2025-07-31 17:27:45 +0900

    feat: esc로 닫기 #11

--- src/search.ts
+++ src/search.ts
@@ -5,14 +5,6 @@ const createPopup = () => {
   const popupWrapper = document.createElement("div");
   popupWrapper.id = SEARCH_POPUP;
 
-  const overlay = document.createElement("div");
-  overlay.id = "overlay";
-  // overlay.addEventListener("click", () => {
-  //   console.log("overlay");
-  // });
-
-  popupWrapper.appendChild(overlay);
-
   const popupContainer = document.createElement("div");
   popupContainer.id = "popup-container";
   popupContainer.innerText = "popup-container";
@@ -22,13 +14,42 @@ const createPopup = () => {
   return popupWrapper;
 };
 
-const deletePopup = () => {
-  const overlay = document.querySelector<HTMLDivElement>(`#overlay`);
-  if (overlay) {
-    // overlay.removeEventListener('click',)
-    // overlay.remove();
+/**
+ * 추가하고 삭제하는 함수의 메모리 주소를 갖게 만들기 위해 이렇게 분류함
+ */
+const handleClickOverlay = (e: MouseEvent) => {
+  e.preventDefault();
+  switch (popupType) {
+    case "search": {
+      deleteOverlay();
+      deletePopup();
+      popupType = "none";
+      break;
+    }
+    case "none": {
+      break;
+    }
+    default: {
+      break;
+    }
   }
+};
+const createOverlay = () => {
+  const overlay = document.createElement("div");
+  overlay.id = "overlay";
+  overlay.addEventListener("click", handleClickOverlay);
+  return overlay;
+};
 
+const deleteOverlay = () => {
+  const overlay = document.querySelector<HTMLDivElement>(`#overlay`);
+  if (!overlay) return;
+
+  overlay.removeEventListener("click", handleClickOverlay);
+  overlay.remove();
+};
+
+const deletePopup = () => {
   const popupWrapper = document.querySelector<HTMLDivElement>(
     `#${SEARCH_POPUP}`,
   );
@@ -44,9 +65,8 @@ const deletePopup = () => {
  * @todo 백드롭 오버레이 추가
  */
 const handlePopup = (e: KeyboardEvent) => {
+  e.preventDefault();
   if ((e.ctrlKey || e.metaKey) && e.key === "k") {
-    e.preventDefault();
-
     const search = document.querySelector<HTMLDivElement>("#search");
 
     if (!search) return;
@@ -54,19 +74,38 @@ const handlePopup = (e: KeyboardEvent) => {
     switch (popupType) {
       case "none": {
         search.appendChild(createPopup());
+        search.appendChild(createOverlay());
 
         popupType = "search";
         break;
       }
       case "search": {
-        deletePopup();
-
-        popupType = "none";
         break;
       }
       default: {
         break;
       }
+    }
+  }
+
+  switch (e.key) {
+    case "Escape": {
+      deleteOverlay();
+      deletePopup();
+      popupType = "none";
+      break;
+    }
+    case "Enter": {
+      console.log("Enter");
+      break;
+    }
+    case "ArrowUp": {
+      console.log("Up arrow pressed");
+      break;
+    }
+    case "ArrowDown": {
+      console.log("Down arrow pressed");
+      break;
     }
   }
 };
