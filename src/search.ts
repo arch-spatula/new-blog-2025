986a8683e555140ffb7a80ecafb4d0ced63cd0fc
김상현 <84452145+arch-spatula@users.noreply.github.com>
2025-07-31 19:20:15 +0900

    feat: 검색 input 추가 #11

--- src/search.ts
+++ src/search.ts
@@ -1,5 +1,42 @@
 let popupType: "none" | "search" = "none";
 
+/**
+ * @todo form 제출 이벤트처리
+ * @todo 아이콘 추가
+ */
+const createSearchInput = () => {
+  const searchForm = document.createElement("form");
+  searchForm.id = "search-form";
+
+  const searchIcon = document.createElement("img");
+  searchIcon.id = "search-icon";
+  searchIcon.src = "/search.svg";
+  searchIcon.alt = "search";
+
+  searchForm.appendChild(searchIcon);
+
+  const searchInput = document.createElement("input");
+  searchInput.id = "search-input";
+  searchInput.type = "search";
+  searchInput.name = "search";
+  searchInput.placeholder = "Search";
+
+  searchForm.appendChild(searchInput);
+
+  const searchAction = document.createElement("div");
+
+  searchForm.appendChild(searchAction);
+
+  return searchForm;
+};
+
+const focusSearchInput = () => {
+  const searchInput = document.querySelector<HTMLInputElement>("#search-input");
+  if (!searchInput) return;
+
+  searchInput.focus();
+};
+
 const SEARCH_POPUP = "search-popup" as const;
 const createPopup = () => {
   const popupWrapper = document.createElement("div");
@@ -7,7 +44,8 @@ const createPopup = () => {
 
   const popupContainer = document.createElement("div");
   popupContainer.id = "popup-container";
-  popupContainer.innerText = "popup-container";
+
+  popupContainer.appendChild(createSearchInput());
 
   popupWrapper.appendChild(popupContainer);
 
@@ -60,13 +98,15 @@ const deletePopup = () => {
   popupWrapper.remove();
 };
 /**
- * popup을 제어함
+ * - e.preventDefault는 조건에 해당할 때만 호출하는 것으로 브라우저에 내장된 단축키를 차단하는 일이 없어야 함.
+ * - popup을 제어함
+ *
  * @todo 이미 popup이 활성화 되어 있으면 popup을 닫음
  * @todo 백드롭 오버레이 추가
  */
 const handlePopup = (e: KeyboardEvent) => {
-  e.preventDefault();
   if ((e.ctrlKey || e.metaKey) && e.key === "k") {
+    e.preventDefault();
     const search = document.querySelector<HTMLDivElement>("#search");
 
     if (!search) return;
@@ -75,6 +115,7 @@ const handlePopup = (e: KeyboardEvent) => {
       case "none": {
         search.appendChild(createPopup());
         search.appendChild(createOverlay());
+        focusSearchInput();
 
         popupType = "search";
         break;
@@ -90,6 +131,7 @@ const handlePopup = (e: KeyboardEvent) => {
 
   switch (e.key) {
     case "Escape": {
+      e.preventDefault();
       deleteOverlay();
       deletePopup();
       popupType = "none";
@@ -110,6 +152,10 @@ const handlePopup = (e: KeyboardEvent) => {
   }
 };
 
+/**
+ * @todo `data.json`은 여기서 접근하기
+ * - 나중에 `data.json`을 접근하는 파일을 단 한 곳에서만 관리하기
+ */
 const main = async () => {
   window.addEventListener("keydown", handlePopup);
   // window.addEventListener("beforeunload", () => {
